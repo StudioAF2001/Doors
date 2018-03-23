91b1495b302fe3e9359ed8171d74c48ff8664179
StudioAF <37297804+StudioAF2001@users.noreply.github.com>
2018-03-23 18:10:26 +0000

    Add files via upload

--- Doors/main.py
+++ Doors/main.py
@@ -23,6 +23,8 @@ white = ((255,255,255))
 black = ((0,0,0))
 grey = ((150,150,150))
 
+
+
 # other required variables
 score = 0
 
@@ -39,26 +41,33 @@ class door:
 def game():
     global score
 
+    highScoreDoc = open("highScore.txt", "r")
+    highScore = int(highScoreDoc.read())
+    highScoreDoc.close
+
+    #Images
+    doorOpenImg = pygame.image.load('img\doorOpen.png')
+    doorClosedImg = pygame.image.load('img\doorClosed.png')
+
     doors = []
     
     myfont = pygame.font.SysFont('Comic Sans MS', 30)
-    textsurface = myfont.render("Score: " +str(0), False, (0, 0, 0))
+    textsurface = myfont.render("Score: %s        Highscore: %s" % (0, highScore), False, white)
     
     mouseDownPast = False
     mouseDownNow = False
 
     score = 0
     
-    # create desired number of doors
-    for i in range(0,5):
-        newDoor = door()
+    # create first number of doors
+    newDoor = door()
 
-        newDoor.size = random.randrange(40, 70)
-        newDoor.x = random.randrange(0, display_width-newDoor.size)
-        newDoor.y = random.randrange(display_height, display_height*2)
-        newDoor.speed = (newDoor.size-30)/5
+    newDoor.size = random.randrange(40, 70)
+    newDoor.x = random.randrange(0, display_width-newDoor.size)
+    newDoor.y = random.randrange(display_height, display_height*2)
+    newDoor.speed = (newDoor.size-30)/5
         
-        doors.append(newDoor)
+    doors.append(newDoor)
 
     # the game loop
     while True:
@@ -80,10 +89,13 @@ def game():
         
 
         # refresh the screen
-        gameDisplay.fill(white)
+        gameDisplay.fill(black)
 
         # move and draw doors
         for i in range(0,len(doors)):
+
+            OpenImg = pygame.transform.scale(doorOpenImg, (doors[i].size, int(doors[i].size*1.5)))
+            ClosedImg = pygame.transform.scale(doorClosedImg, (doors[i].size, int(doors[i].size*1.5)))
 
             openchance = random.randrange(doors[i].speed*5,70)
             if doors[i].y < display_height and openchance > 68:
@@ -95,32 +107,51 @@ def game():
                     doors[i].doorOpen = True
             
             doors[i].y -= doors[i].speed
-            
-            if doors[i].y < 0-(doors[i].size*1.5):
-                return
 
             if mouseX >= doors[i].x and mouseX <= doors[i].x + doors[i].size:
                 if mouseY >= doors[i].y and mouseY <= doors[i].y + doors[i].size*1.5:
                     if mouseDownNow == True and mouseDownPast == False:
                         if doors[i].doorOpen:
                             score+=1
-                            textsurface = myfont.render("Score: " +str(score), False, (0, 0, 0))
+                            if score > highScore:
+                                highScore = score
+                                highScoreDoc = open("highScore.txt", "w")
+                                highScoreDoc.write(str(highScore))
+                                highScoreDoc.close
 
+                            textsurface = myfont.render("Score: %s        Highscore: %s" % (score, highScore), False, white)
+                                
                             doors[i].size = random.randrange(40, 70)
                             doors[i].y = display_height
                             doors[i].x = random.randrange(0, display_width-newDoor.size)
                             doors[i].speed = (doors[i].size-35)/5
 
                             doors[i].doorOpen = False
+
+                            if score>0 and score%5 == 0:
+                                newDoor = door()
+
+                                newDoor.size = random.randrange(40, 70)
+                                newDoor.x = random.randrange(0, display_width-newDoor.size)
+                                newDoor.y = random.randrange(display_height, display_height*2)
+                                newDoor.speed = (newDoor.size-30)/5
+                            
+                                doors.append(newDoor)
                         
 
             if doors[i].doorOpen:
                 pygame.draw.rect(gameDisplay, grey, pygame.Rect(doors[i].x,doors[i].y,doors[i].size, doors[i].size*1.5))
+                gameDisplay.blit(OpenImg, (doors[i].x,doors[i].y))
             else:
                 pygame.draw.rect(gameDisplay, black, pygame.Rect(doors[i].x,doors[i].y,doors[i].size, doors[i].size*1.5))
+                gameDisplay.blit(ClosedImg, (doors[i].x,doors[i].y))
+
+            if doors[i].y < 0-(doors[i].size*1.5):
+                pygame.display.update()
+                return
         
         mouseDownPast = mouseDownNow
-
+        
         gameDisplay.blit(textsurface,(0,0))
         
         
@@ -130,7 +161,7 @@ def game():
 
 def gameOver():
     myfont = pygame.font.SysFont('Comic Sans MS', 50)
-    textsurface = myfont.render('''Game Over - Score: %s''' % (score), False, (0, 0, 0))
+    textsurface = myfont.render('''Game Over - Score: %s''' % (score), False, white)
     gameDisplay.blit(textsurface,(70,70))
     pygame.display.update()
 
